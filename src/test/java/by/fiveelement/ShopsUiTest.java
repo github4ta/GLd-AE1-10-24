bfad4998e466550187317761456f986f5e344101
Pavel <pamitrovel@gmail.com>
2024-11-13 20:24:13 +0300

    add WebElements dropdownCities, dropdownCitiesMinskOption, buttonExitCityPopup, footerAddresses, buttonListAddresses

--- src/test/java/by/fiveelement/ShopsUiTest.java
+++ src/test/java/by/fiveelement/ShopsUiTest.java
@@ -2,11 +2,18 @@ package by.fiveelement;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ShopsUiTest {
     private static WebDriver driver;
+    WebElement dropdownCities = driver.findElement(By.xpath("//*[@id=\"app\"]/header/div[1]/div[1]/div/div/div[1]/div/div[1]"));
+    WebElement dropdownCitiesMinskOption = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div/div[2]/div/div[1]/div[1]"));
+    WebElement buttonExitCityPopup = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/button"));
+    WebElement footerAddresses = driver.findElement(By.xpath("//*[@id=\"app\"]/footer/div[2]/div/div/div[1]/div[1]/ul/li[2]/a"));
+    WebElement buttonListAddresses = driver.findElement(By.xpath("//button[@class='nav-link active']"));
 
     @BeforeEach
     public void setUp() {
@@ -19,5 +26,4 @@ public class ShopsUiTest {
     public void tearDown() {
         driver.quit();
     }
-
 }
