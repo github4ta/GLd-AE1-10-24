f1cc40a6a575e6f29515017945ddf8cb1bfb58b8
Pavel <pamitrovel@gmail.com>
2024-11-13 23:00:02 +0300

    add import org.junit.jupiter.api.Assertions;

--- src/test/java/by/fiveelement/ShopsUiTest.java
+++ src/test/java/by/fiveelement/ShopsUiTest.java
@@ -1,6 +1,7 @@
 package by.fiveelement;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
