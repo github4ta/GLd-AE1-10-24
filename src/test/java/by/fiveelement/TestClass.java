173c40ca99fbfc04b8a1aeb9a39f05af05a0160c
Arseni Aleksandrovich <theintosky@gmail.com>
2024-11-13 22:15:31 +0300

    Create main tests

--- src/test/java/by/fiveelement/TestClass.java
+++ src/test/java/by/fiveelement/TestClass.java
@@ -1,4 +1,16 @@
 package by.fiveelement;
 
 public class TestClass {
+    String rawHeaders = "accept:*/*\n" +
+            "accept-language:ru-BY,ru;q=0.9,en-US;q=0.8,en;q=0.7,ru-RU;q=0.6,bg;q=0.5,uk;q=0.4\n" +
+            "origin:https://5element.by\n" +
+            "priority:u=1, i\n" +
+            "referer:https://5element.by/\n" +
+            "sec-ch-ua:\"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"\n" +
+            "sec-ch-ua-mobile:?0\n" +
+            "sec-ch-ua-platform:\"Windows\"\n" +
+            "sec-fetch-dest:empty\n" +
+            "sec-fetch-mode:cors\n" +
+            "sec-fetch-site:cross-site\n" +
+            "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36";
 }
--- src/test/java/by/fiveelement/UserLoginUiTest.java
+++ src/test/java/by/fiveelement/UserLoginUiTest.java
@@ -1,25 +1,56 @@
 package by.fiveelement;
 
 import jdk.jfr.Description;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+import java.time.Duration;
 
 public class UserLoginUiTest {
     private WebDriver webDriver;
+
     private By loginButtonLocator = By.xpath("//a[@class='h-drop h-user']/div/div[@class='h-drop__text'][contains(text(),'Вход')]");
     private By secondLoginButtonLocator = By.xpath("//li[@class='protection-tab active']");
     private By loginFieldLocator = By.xpath("//input[@name='login']");
     private By passwordFieldLocator = By.xpath("//input[@type='password']");
     private By enterButtonLocator = By.xpath("//div[@class='modal-popup-form login-form']/*/*/button");
+    private By errorMessageLocator = By.xpath("//span[@class='inp-required validation-text']");
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        webDriver.manage().window().maximize();
         webDriver.get("https://5element.by/");
         webDriver.findElement(loginButtonLocator).click();
         webDriver.findElement(secondLoginButtonLocator).click();
+    }
+
+    @Test
+    @Description("Unregistered email")
+    public void unregisteredEmailTest() {
+        webDriver.findElement(loginFieldLocator).sendKeys("test@test.com");
+        webDriver.findElement(passwordFieldLocator).sendKeys("123456");
+        webDriver.findElement(enterButtonLocator).click();
+        Assertions.assertEquals("Пользователь не найден",webDriver.findElement(errorMessageLocator).getText());
+    }
+
+    @Test
+    @Description("Wrong password")
+    public void wrongPasswordTest() {
+        webDriver.findElement(loginFieldLocator).sendKeys("+375290000000");
+        webDriver.findElement(passwordFieldLocator).sendKeys("123456");
+        webDriver.findElement(enterButtonLocator).click();
+        Assertions.assertEquals("Неверный пароль",webDriver.findElement(errorMessageLocator).getText());
+    }
+
+    @AfterTest
+    public void tearDown() {
+        webDriver.quit();
     }
 }
