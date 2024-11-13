c4e3143bd2104c84873d752639bb56c40eef2576
nstsy <nstsy@yandex.ru>
2024-11-13 20:41:56 +0300

    add methods(BeforeEach, AfterEach) for BaseTest

--- src/test/java/by/fiveelement/BaseTest.java
+++ src/test/java/by/fiveelement/BaseTest.java
@@ -1,4 +1,25 @@
 package by.fiveelement;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class BaseTest {
+    protected WebDriver webDriver;
+
+    @BeforeEach
+    public void setUp() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        webDriver.get("https://5element.by/catalog/377-smartfony");
+        webDriver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        webDriver.quit();
+    }
 }
