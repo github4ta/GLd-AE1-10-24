d86aa2cd71c32cc5a5a30fda91b61d1bd447c3cf
palinaerdman <polinaerdman@gmail.com>
2024-11-13 20:52:31 +0300

    refactor UserRegistrationApiTest (add test "Регистрация без имени и номера телефона")

--- src/test/java/by/fiveelement/UserRegistrationApiTest.java
+++ src/test/java/by/fiveelement/UserRegistrationApiTest.java
@@ -2,7 +2,6 @@ package by.fiveelement;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -25,6 +24,27 @@ public class UserRegistrationApiTest {
         .then()
                 .log().all()
                 .statusCode(200)
+                .body("errors.fields.mobile", equalTo("Укажите номер телефона"));
+    }
+
+    @Test
+    @DisplayName("Регистрация без имени и номера телефона")
+    public void testRegistrationWithoutNameAndPhoneNumber() {
+        String body = "{\n" +
+                "    \"name\": \"\",\n" +
+                "    \"mobile\": \"\",\n" +
+                "    \"type\": \"reg_code\"\n" +
+                "}";
+        String url = "https://5element.by/user/registration";
+        given()
+                .body(body)
+                .headers("Content-Type", "application/json")
+                .when()
+                .post(url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("errors.fields.name", equalTo("Укажите имя"))
                 .body("errors.fields.mobile", equalTo("Укажите номер телефона"));
     }
 }
