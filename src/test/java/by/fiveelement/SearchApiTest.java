a8e5affe1c64ad93f2bc77685fbf4541b4852348
baidakm <maksimbaidak@gmail.com>
2024-11-13 19:06:01 +0300

    add searchStatusCodeTest

--- src/test/java/by/fiveelement/SearchApiTest.java
+++ src/test/java/by/fiveelement/SearchApiTest.java
@@ -1,5 +1,10 @@
 package by.fiveelement;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+
 public class SearchApiTest {
 
     private static String URL = "https://sort.diginetica.net/search?st=apple&apiKey=08IE0509XQ&strategy=advanced_xname,zero_queries&fullData=true&withCorrection=true&withFacets=true&treeFacets=true&regionId=global&useCategoryPrediction=false&size=20&offset=0&showUnavailable=true&unavailableMultiplier=0.002&preview=false&withSku=false&sort=DEFAULT";
@@ -16,4 +21,18 @@ public class SearchApiTest {
             "sec-fetch-mode:cors\n" +
             "sec-fetch-site:cross-site\n" +
             "user-agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36";
+
+    @Test
+    void searchStatusCodeTest(){
+        RestAssured
+                .given()
+                    .headers(rawHeaders
+                                .lines()
+                                .map(line -> line.split(":"))
+                                .collect(Collectors.toMap(key -> key[0], val -> val[1])))
+                .when()
+                    .get(URL)
+                .then()
+                    .statusCode(200);
+    }
 }
