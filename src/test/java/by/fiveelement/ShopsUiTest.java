d1749380d035b8accf79de9fb677eef5090adf4d
Pavel <pamitrovel@gmail.com>
2024-11-13 22:59:35 +0300

    add checkShopListTest

--- src/test/java/by/fiveelement/ShopsUiTest.java
+++ src/test/java/by/fiveelement/ShopsUiTest.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ShopsUiTest {
     protected WebDriver driver;
@@ -38,12 +41,54 @@ public class ShopsUiTest {
         return this;
     }
 
+    public static final String[] requiredAddressesList = {
+            "ул. Корженевского, 26 (ТЦ «Корона», 1 этаж)",
+            "пр-т Победителей, 65/1 (ТЦ «Замок», 4 этаж)",
+            "пр-т Независимости, 154 (ТЦ «Корона», 1 этаж)",
+            "ул. Кальварийская, 24 (ТЦ «Корона», 2 этаж)",
+            "ул. Денисовская, 8 (ТЦ «Корона-Сити», 1 этаж)",
+            "ул. Монтажников, 2 (Гипермаркет «Евроопт», 1 этаж)",
+            "пр-т Партизанский, 79 (ТЦ «Prizma», цокольный этаж)",
+            "пр-т Дзержинского, 104-2 (ТЦ «Титан», 1 этаж)",
+            "ул. Голубка, 2 (ТЦ «Бонус», 2 этаж)",
+            "ул. Горецкого, 2 (ТЦ «Гиппо», 1 этаж)",
+            "д. Боровая, 7а (ТЦ «Боро», 1 этаж)",
+            "ул. Мстиславца, 11 (ТЦ «Dana Mall», 3 этаж)",
+            "пр-т Победителей, 9 (ТРЦ «Галерея Минск», 4 этаж)",
+            "пр. Независимости, 117",
+            "ул. Нестерова, 49 (Гипермаркет «Евроопт», 2 этаж)",
+            "ул. Притыцкого, 156 (ТЦ «GreenCity», 2 этаж)",
+            "ул. Кульман, 14-1 (1 этаж)"
+    };
+
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://5element.by/");
+    }
+
+    @Test
+    public void checkShopListTest() {
+        clickDropdownCities()
+                .clickDropdownCitiesMinskOption()
+                .clickFooterAddresses()
+                .clickButtonListAddresses();
+
+        List<WebElement> addressElements = driver.findElements(By.xpath("//b[@class='si-name']"));
+
+        Set<String> actualAddresses = new HashSet<>();
+        for (WebElement addressElement : addressElements) {
+            actualAddresses.add(addressElement.getText());
+        }
+
+        Set<String> expectedAddresses = new HashSet<>();
+        for (String expectedAddress : requiredAddressesList) {
+            expectedAddresses.add(expectedAddress);
+        }
+
+        Assertions.assertEquals(expectedAddresses, actualAddresses);
     }
 
     @AfterEach
