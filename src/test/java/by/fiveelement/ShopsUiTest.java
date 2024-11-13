86246d67269a4ebe0c83bb558c55bc5e8234081a
Pavel <pamitrovel@gmail.com>
2024-11-13 22:37:28 +0300

    add DROPDOWN_CITIES_XPATH, DROPDOWN_OPTION_CITIES_MINSK_XPATH, FOOTER_ADDRESSES_XPATH, BUTTON_LIST_ADDRESSES_XPATH  clickDropdownCities, clickDropdownCitiesMinskOption, clickFooterAddresses, clickButtonListAddresses

--- src/test/java/by/fiveelement/ShopsUiTest.java
+++ src/test/java/by/fiveelement/ShopsUiTest.java
@@ -2,22 +2,46 @@ package by.fiveelement;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class ShopsUiTest {
-    private static WebDriver driver;
-    WebElement dropdownCities = driver.findElement(By.xpath("//*[@id=\"app\"]/header/div[1]/div[1]/div/div/div[1]/div/div[1]"));
-    WebElement dropdownCitiesMinskOption = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div/div[2]/div/div[1]/div[1]"));
-    WebElement buttonExitCityPopup = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/button"));
-    WebElement footerAddresses = driver.findElement(By.xpath("//*[@id=\"app\"]/footer/div[2]/div/div/div[1]/div[1]/ul/li[2]/a"));
-    WebElement buttonListAddresses = driver.findElement(By.xpath("//button[@class='nav-link active']"));
+    protected WebDriver driver;
+
+    public static final String DROPDOWN_CITIES_XPATH = "//span[@class='ic-h-location']";
+    public static final String DROPDOWN_OPTION_CITIES_MINSK_XPATH = "//div[@class='option select-open__highlight checked']";
+    public static final String FOOTER_ADDRESSES_XPATH = "//*[@id=\"app\"]/footer/div[2]/div/div/div[1]/div[1]/ul/li[2]/a";
+    public static final String BUTTON_LIST_ADDRESSES_XPATH = "//button[@class='nav-link active']";
+
+    public ShopsUiTest clickDropdownCities() {
+        driver.findElement(By.xpath(DROPDOWN_CITIES_XPATH)).click();
+        return this;
+    }
+
+    public ShopsUiTest clickDropdownCitiesMinskOption() {
+        driver.findElement(By.xpath(DROPDOWN_OPTION_CITIES_MINSK_XPATH)).click();
+        return this;
+    }
+
+    public ShopsUiTest clickFooterAddresses() {
+        driver.findElement(By.xpath(FOOTER_ADDRESSES_XPATH)).click();
+        return this;
+    }
+
+    public ShopsUiTest clickButtonListAddresses() {
+        driver.findElement(By.xpath(BUTTON_LIST_ADDRESSES_XPATH)).click();
+        return this;
+    }
 
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://5element.by/");
     }
