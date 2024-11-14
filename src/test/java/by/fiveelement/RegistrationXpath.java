31e4efc7d93c7ac808b24b0316d106852515ba6e
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-14 15:00:57 +0300

    add Xpaths from registration form

--- src/test/java/by/fiveelement/RegistrationXpath.java
+++ src/test/java/by/fiveelement/RegistrationXpath.java
@@ -1,4 +1,11 @@
 package by.fiveelement;
 
 public class RegistrationXpath {
+
+    public static final String EnterButtonXpath = "//*[@id=\"app\"]/header/div[1]/div[1]/div/div/div[4]/a[2]/div";
+    public static final String EnterRegistrationButtonXpath = "//*[@id=\"app\"]/div[16]/div/div[2]/ul/li[2]";
+    public static final String InputName = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[1]/input";
+    public static final String InputPhone = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[2]/input";
+    public static final String getCodeInViberOrSms = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[4]/button";
+
 }
