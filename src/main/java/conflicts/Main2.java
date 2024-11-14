e53455a2e86301ecd3e7d228c189fa386e7f85c8
Arseni <137502512+FoXCity1337@users.noreply.github.com>
2024-11-14 21:40:56 +0300

    aleksandrovich-conflict2 (#33)
    * add selenium
    
    * Create UserLoginUiTest class and setUp method
    
    * Create main tests
    
    * Create incorrectInputTest
    
    * bug fix
    
    * clear TestClass
    
    * Add hello world

--- src/main/java/conflicts/Main2.java
+++ src/main/java/conflicts/Main2.java
@@ -4,6 +4,7 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello 2 from Sergei Tsarik.");
         System.out.println("Hello 2 from Ekaterina Karpovich!");
+        System.out.println("Hello World!");
         System.out.println("Hello 2 from Viktoria Zakarian.");
         System.out.println("Hello 2 from Aliaksandr Sianiuk.");
         System.out.println("Hello 2 from Ekaterina Karpovich.");
