8b06584f9dc142c7992df7c921555604fc934d97
baidakm <maksimbaidak@gmail.com>
2024-11-14 10:55:20 +0300

    refactor searchStatusCodeTest

--- src/test/java/by/fiveelement/SearchApiTest.java
+++ src/test/java/by/fiveelement/SearchApiTest.java
@@ -3,13 +3,38 @@ package by.fiveelement;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Collectors;
+
 public class SearchApiTest {
 
-    private static String URL = "https://sort.diginetica.net/search?st=apple&apiKey=08IE0509XQ&strategy=advanced_xname,zero_queries&fullData=true&withCorrection=true&withFacets=true&treeFacets=true&regionId=global&useCategoryPrediction=false&size=20&offset=0&showUnavailable=true&unavailableMultiplier=0.002&preview=false&withSku=false&sort=DEFAULT";
+    private String URL = "https://sort.diginetica.net/search";
+
+    private String rawParams = "st:apple\n" +
+            "apiKey:08IE0509XQ\n" +
+            "strategy:advanced_xname,zero_queries\n" +
+            "fullData:true\n" +
+            "withCorrection:true\n" +
+            "withFacets:true\n" +
+            "treeFacets:true\n" +
+            "regionId:global\n" +
+            "useCategoryPrediction:false\n" +
+            "size:20\n" +
+            "offset:0\n" +
+            "showUnavailable:true\n" +
+            "unavailableMultiplier:0.002\n" +
+            "preview:false\n" +
+            "withSku:false\n" +
+            "sort:DEFAULT";
 
     @Test
     void searchStatusCodeTest(){
         RestAssured
+                .given()
+                    .basePath(URL)
+                    .queryParams(rawParams
+                                    .lines()
+                                    .map(line -> line.split(":"))
+                                    .collect(Collectors.toMap(key -> key[0], val -> val[1])))
                 .when()
                     .get(URL)
                 .then()
