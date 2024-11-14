8a8ffa2e4a43baabb26731e1f3ed0be02607107c
Yuliya <yuliya.01707@gmail.com>
2024-11-14 18:37:31 +0100

    update conflict

--- src/main/java/conflicts/Main.java
+++ src/main/java/conflicts/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Sergei Tsarik!");
     }
+    public static void main(String[] args) {
+        System.out.println("Hello from Yuliya Shabaikovich!");
+    }
+
 }
