2f3298d83c1f40642b36523911554172f849ede6
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-14 21:37:13 +0300

    refactor conflicts 2 (#32)

--- src/main/java/conflicts/Main2.java
+++ src/main/java/conflicts/Main2.java
@@ -3,8 +3,7 @@ package conflicts;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("Hello 2 from Sergei Tsarik.");
-        System.out.println("What is it?");
-        System.out.println("Hello 2 from Artsiom Mitskevich!");
+        System.out.println("Hello 2 from Viktoria Zakarian.");
         System.out.println("Hello 2 from Aliaksandr Sianiuk.");
         System.out.println("Hello 2 from Ekaterina Karpovich.");
         System.out.println("Hello from Eugene Yarosh! This is conflict 2!");
