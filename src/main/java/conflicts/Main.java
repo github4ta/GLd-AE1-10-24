de4961074296216a540203a1355f861afb083260
nstsy <nstsy@yandex.ru>
2024-11-14 20:32:45 +0300

    add main conflict

--- src/main/java/conflicts/Main.java
+++ src/main/java/conflicts/Main.java
@@ -2,6 +2,6 @@ package conflicts;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Sergei Tsarik!");
+        System.out.println("Hello from Stolybko!");
     }
 }
