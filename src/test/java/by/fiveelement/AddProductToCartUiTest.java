1bc4086a117b68d986d2e67bb8948dc469c6d545
Nstsy <nstsy@yandex.ru>
2024-11-14 11:11:58 +0300

    5E UI 006 Тестирование добавления товара в корзину (#7)
    * add classes for testing
    
    * add methods(BeforeEach, AfterEach) for BaseTest
    
    * add test
    
    * refactor test
    
    * delete BaseTest

---
+++ src/test/java/by/fiveelement/AddProductToCartUiTest.java
@@ -0,0 +1,37 @@
+package by.fiveelement;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class AddProductToCartUiTest {
+    WebDriver webDriver = new ChromeDriver();
+    @BeforeEach
+    public void setUp() {
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        webDriver.get("https://5element.by/");
+        webDriver.manage().window().maximize();
+    }
+
+    @Test
+    public void addProductToCartTest() {
+        CartPage cartPage = new CartPage(webDriver);
+
+        cartPage.clickButtonCatalog()
+                .clickButtonSmartphonesAndGadgets()
+                .clickButtonSmartphones()
+                .clickButtonAddToCart();
+
+        Assertions.assertTrue(cartPage.getTextAboutSmartphone().contains(CartPageMessage.TEXT_ADD_TO_CART));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        webDriver.quit();
+    }
+}
---
+++ src/test/java/by/fiveelement/CartPageXPath.java
@@ -0,0 +1,10 @@
+package by.fiveelement;
+
+public class CartPageXPath {
+    public static final String BUTTON_CATALOG_XPATH = "//*[@id=\"app\"]/header/div[1]/div[2]/div/div[1]/div[1]/div[2]/div[1]/button";
+    public static final String BUTTON_SMARTPHONES_AND_GADGETS_XPATH = "//*[@id=\"app\"]/header/div[2]/div[2]/div/div/div[1]/ul/li[3]";
+    public static final String MENU_SMARTPHONES_AND_GADGETS_XPATH = "//*[@id=\"app\"]/main/div[2]/div/div/div/a[1]";
+    public static final String BUTTON_SMARTPHONES_XPATH = "//*[@id=\"app\"]/main/div[2]/div/div/div/a[1]/ul/li[2]/a";
+    public static final String BUTTON_ADD_TO_CART_XPATH = "//*[@id=\"app\"]/main/div[3]/div/div[1]/div[2]/div[2]/div/div/div[1]/div[1]/div/div[3]/div[4]/button/span";
+    public static final String TEXT_ADD_TO_CART_XPATH = "//*[@id=\"app\"]/div[11]/div/div[1]/div";
+}
