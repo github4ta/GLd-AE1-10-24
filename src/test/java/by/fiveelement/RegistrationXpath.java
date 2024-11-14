6694fca47bf7770e94c1a57b34989a472061603b
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-14 16:59:26 +0300

    add Xpaths

--- src/test/java/by/fiveelement/RegistrationXpath.java
+++ src/test/java/by/fiveelement/RegistrationXpath.java
@@ -2,10 +2,12 @@ package by.fiveelement;
 
 public class RegistrationXpath {
 
-    public static final String EnterButtonXpath = "//*[@id=\"app\"]/header/div[1]/div[1]/div/div/div[4]/a[2]/div";
-    public static final String EnterRegistrationButtonXpath = "//*[@id=\"app\"]/div[16]/div/div[2]/ul/li[2]";
-    public static final String InputName = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[1]/input";
-    public static final String InputPhone = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[2]/input";
-    public static final String getCodeInViberOrSms = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[4]/button";
-
+    public static final String ENTER_BUTTON_XPATH = "//*[@id=\"app\"]/header/div[1]/div[1]/div/div/div[4]/a[2]/div";
+    public static final String ENTER_REGISTRATION_BUTTON_XPATH = "//*[@id=\"app\"]/div[16]/div/div[2]/ul/li[2]";
+    public static final String INPUT_NAME_XPATH = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[1]/input";
+    public static final String INPUT_PHONE_XPATH = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[2]/input";
+    public static final String GET_CODE_IN_VIBER_OR_SMS_XPATH = "//*[@id=\"app\"]/div[16]/div/div[2]/form/div[4]";
+    public static final String CHECK_SEND_CODE_XPATH = "//*[@id=\"app\"]/div[16]/div/div[1]/div";
+    public static final String CHECK_SEND_ON_NUMBER = "//*[@id=\"app\"]/div[16]/div/div[2]/div";
+    public static final String ACCEPT_COCKIE = "//*[@id=\"modal-cookies\"]/div/div/div/div/div[2]/button[2]";
 }
