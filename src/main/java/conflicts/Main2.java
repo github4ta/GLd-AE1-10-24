5c8f0e5a008d0d8fc7a2becf171efc3f32dfb674
nstsy <nstsy@yandex.ru>
2024-11-14 21:04:23 +0300

    add main conflict2

--- src/main/java/conflicts/Main2.java
+++ src/main/java/conflicts/Main2.java
@@ -2,6 +2,6 @@ package conflicts;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("Hello 2 from Sergei Tsarik.");
+        System.out.println("Hello 2 from Stolybko");
     }
 }
