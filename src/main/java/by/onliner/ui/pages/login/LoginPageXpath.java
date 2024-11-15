7d4049addb752eea2bb8262399cae0d1a3be58d7
nstsy <nstsy@yandex.ru>
2024-11-15 18:59:30 +0300

    добавлены локаторы для некоторых веб-элементов логин формы

--- src/main/java/by/onliner/ui/pages/login/LoginPageXpath.java
+++ src/main/java/by/onliner/ui/pages/login/LoginPageXpath.java
@@ -1,4 +1,9 @@
 package by.onliner.ui.pages.login;
 
 public class LoginPageXpath {
+    public static final String BUTTON_CLOSE_XPATH = "//div[@class='auth-form__close']";
+    public static final String TEXT_XPATH = "(//div[@class='auth-form__label-title'])[2]";
+    public static final String INPUT_NAME_XPATH = "//input[@wfd-id='id1']";
+    public static final String INPUT_PASSWORD_XPATH = "//input[@wfd-id='id2']";
+    public static final String BUTTON_LOGIN_XPATH = "//button[@type='submit']";
 }
