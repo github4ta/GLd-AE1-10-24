beed4ac8b2f85dbeb8a3c8d18b5384c05f93b97c
Pavel <pamitrovel@gmail.com>
2024-11-15 17:08:34 +0300

    add FOOTER_LINK_PERSONAL_DATA_PROCESSING_POLICY_XPATH, FOOTER_LINK_USER_SUPPORT_XPATH, FOOTER_LINK_RETURN_POLICY_XPATH

--- src/main/java/by/onliner/ui/pages/home/HomePageXpath.java
+++ src/main/java/by/onliner/ui/pages/home/HomePageXpath.java
@@ -1,4 +1,7 @@
 package by.onliner.ui.pages.home;
 
 public class HomePageXpath {
+    public static final String FOOTER_LINK_PERSONAL_DATA_PROCESSING_POLICY_XPATH = "//li[@class='footer-style__item'][9]";
+    public static final String FOOTER_LINK_USER_SUPPORT_XPATH = "//li[@class='footer-style__item'][10]";
+    public static final String FOOTER_LINK_RETURN_POLICY_XPATH = "///li[@class='footer-style__item'][11]";
 }
