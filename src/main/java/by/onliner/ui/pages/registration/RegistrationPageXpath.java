4c140cfa92fff8b466f923809a6bc6457822fd2c
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-15 19:00:12 +0300

    add registration form Xpaths

--- src/main/java/by/onliner/ui/pages/registration/RegistrationPageXpath.java
+++ src/main/java/by/onliner/ui/pages/registration/RegistrationPageXpath.java
@@ -1,4 +1,16 @@
 package by.onliner.ui.pages.registration;
 
 public class RegistrationPageXpath {
+
+    public static final String PERSONAL_DATA_POLICY_XPATH = "//a[@href='https://blog.onliner.by/politika-konfidencialnosti']";
+    public static final String SITE_RULES_XPATH = "//a[@href='https://blog.onliner.by/siterules']";
+    public static final String SUBJECT_RIGHTS_XPATH = "//a[@href='https://blog.onliner.by/prava-subekta-personalnih-dannih']";
+    public static final String TRANSFER_PERSONAL_DATA_XPATH = "//a[@href='https://blog.onliner.by/celi-i-uslovua-transgranichnoi-peredachi-dannih']";
+    public static final String GIVING_OR_REFUSING_CONSENT_XPATH = "//a[@href='https://blog.onliner.by/posledstvia-dachi-soglasia']";
+    public static final String ACCEPT_PROCESSING_PERSONAL_DATA_TEXT_XPATH = "//span[@class='auth-form__checkbox-sign']";
+    public static final String ACCEPT_PROCESSING_PERSONAL_DATA_CHECKBOX_XPATH = "//span[@class='auth-checkbox__faux']";
+    public static final String REGISTRATION_BUTTON_XPATH = "//button[@type='submit']";
+    public static final String LOGIN_BUTTON_XPATH = "//a[@href='/login']";
+    public static final String DO_YOU_HAVE_PROFILE_TEXT_XPATH = "//div[@class='auth-form__description auth-form__description_primary auth-form__description_small auth-form__description_condensed-other']";
+    public static final String COPYRIGHT_TEXT_XPATH = "//div[@class='auth-form__description auth-form__description_other auth-form__description_small auth-form__description_condensed']";
 }
