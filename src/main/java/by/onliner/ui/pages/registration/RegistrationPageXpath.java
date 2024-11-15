29b1056cf8f0f61dc8fd12234320e43999ec9a8e
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-15 21:28:34 +0300

    fix indentation in Value Xpath

--- src/main/java/by/onliner/ui/pages/registration/RegistrationPageXpath.java
+++ src/main/java/by/onliner/ui/pages/registration/RegistrationPageXpath.java
@@ -7,6 +7,6 @@ public class RegistrationPageXpath {
     public static final String ACCEPT_PROCESSING_PERSONAL_DATA_CHECKBOX_XPATH = "//span[@class='auth-checkbox__faux']";
     public static final String REGISTRATION_BUTTON_XPATH = "//button[@type='submit']";
     public static final String LOGIN_BUTTON_XPATH = "//a[@href='/login']";
-    public static final String DO_YOU_HAVE_PROFILE_TEXT_XPATH = "//div[contains(text(),'          Есть профиль на Onlíner?')]";
+    public static final String DO_YOU_HAVE_PROFILE_TEXT_XPATH = "//div[contains(text(),'Есть профиль на Onlíner?')]";
     public static final String COPYRIGHT_TEXT_XPATH = "//div[@class='auth-form__footer']";
 }
