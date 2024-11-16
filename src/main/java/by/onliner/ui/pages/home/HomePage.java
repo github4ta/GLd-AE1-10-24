936df639fef18a305f57ae35d1939b4adccc42c6
Sergei Tsarik <serge.tsarik@gmail.com>
2024-11-16 15:18:55 +0300

    add home page methods class

--- src/main/java/by/onliner/ui/pages/home/HomePage.java
+++ src/main/java/by/onliner/ui/pages/home/HomePage.java
@@ -1,4 +1,13 @@
 package by.onliner.ui.pages.home;
 
+import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage() {
+        driver = Driver.getDriver();
+    }
 }
