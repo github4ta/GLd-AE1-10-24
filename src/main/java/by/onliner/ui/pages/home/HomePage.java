5e9a9a3121e414322d6ae69184305a49a9f8081b
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-16 21:43:58 +0300

    refactor HomePage add methods getTextEditorContacts, clickLinkAdvertising

--- src/main/java/by/onliner/ui/pages/home/HomePage.java
+++ src/main/java/by/onliner/ui/pages/home/HomePage.java
@@ -1,6 +1,7 @@
 package by.onliner.ui.pages.home;
 
 import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -9,5 +10,14 @@ public class HomePage {
 
     public HomePage() {
         driver = Driver.getDriver();
+    }
+
+    public String getTextEditorContacts() {
+        return driver.findElement(By.xpath(HomePageXpath.EDITOR_CONTACTS_XPATH)).getText();
+    }
+
+    public HomePage clickLinkAdvertising() {
+        driver.findElement(By.xpath(HomePageXpath.ADVERTISING_XPATH)).click();
+        return this;
     }
 }
