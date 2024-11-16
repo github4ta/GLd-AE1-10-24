23f1fee27b35b675eaaed04a04d7f640b497980f
nstsy <nstsy@yandex.ru>
2024-11-16 07:00:36 +0300

    перименовал локатор

--- src/main/java/by/onliner/ui/pages/login/LoginPageXpath.java
+++ src/main/java/by/onliner/ui/pages/login/LoginPageXpath.java
@@ -2,7 +2,7 @@ package by.onliner.ui.pages.login;
 
 public class LoginPageXpath {
     public static final String BUTTON_CLOSE_XPATH = "//div[@class='auth-form__close']";
-    public static final String TEXT_XPATH = "(//div[@class='auth-form__label-title'])[2]";
+    public static final String FIELD_TITLE_USERNAME_XPATH = "(//div[@class='auth-form__label-title'])[2]";
     public static final String INPUT_NAME_XPATH = "//input[@wfd-id='id1']";
     public static final String INPUT_PASSWORD_XPATH = "//input[@wfd-id='id2']";
     public static final String BUTTON_LOGIN_XPATH = "//button[@type='submit']";
