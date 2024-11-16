be0250ab638b8b2d8f3afcce134f3732d6e8fd39
Arseni Aleksandrovich <theintosky@gmail.com>
2024-11-17 00:47:37 +0300

    add getTextFromAboutCompanyElement and clickEditorContacts methods

--- src/main/java/by/onliner/ui/pages/home/HomePage.java
+++ src/main/java/by/onliner/ui/pages/home/HomePage.java
@@ -1,6 +1,7 @@
 package by.onliner.ui.pages.home;
 
 import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -9,5 +10,14 @@ public class HomePage {
 
     public HomePage() {
         driver = Driver.getDriver();
+    }
+
+    public String getTextFromAboutCompanyElement() {
+        return Driver.getDriver().findElement(By.xpath(HomePageXpath.ABOUT_COMPANY_XPATH)).getText();
+    }
+
+    public HomePage clickEditorContacts() {
+        Driver.getDriver().findElement(By.xpath(HomePageXpath.EDITOR_CONTACTS_XPATH)).click();
+        return this;
     }
 }
