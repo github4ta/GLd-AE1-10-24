ea8eb6d4b36617e1150c700d3a5f71c09148e04b
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-11-16 23:58:51 +0300

    add methods getTextAdvertisingLink(), clickTariffsLink()

--- src/main/java/by/onliner/ui/pages/home/HomePage.java
+++ src/main/java/by/onliner/ui/pages/home/HomePage.java
@@ -1,6 +1,7 @@
 package by.onliner.ui.pages.home;
 
 import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -9,5 +10,14 @@ public class HomePage {
 
     public HomePage() {
         driver = Driver.getDriver();
+    }
+
+    public String getTextAdvertisingLink() {
+        return driver.findElement(By.xpath(HomePageXpath.ADVERTISING_LINK_XPATH)).getText();
+    }
+
+    public HomePage clickTariffsLink() {
+        driver.findElement(By.xpath(HomePageXpath.TARIFFS_LINK_XPATH)).click();
+        return this;
     }
 }
