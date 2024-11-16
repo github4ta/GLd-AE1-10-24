ea32be86581335467e8527ded003366e0410cb92
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-16 12:26:55 +0300

    refactor XPATH form Registration

--- src/main/java/by/onliner/ui/pages/registration/RegistrationPageXpath.java
+++ src/main/java/by/onliner/ui/pages/registration/RegistrationPageXpath.java
@@ -4,6 +4,6 @@ public class RegistrationPageXpath {
     public static final String MESSAGE_OR_BY_EMAIL_XPATH = "//div[@class=\"auth-form__label-title\"][text()='или через e-mail']";
     public static final String INPUT_EMAIL_XPATH = "//input[@type=\"email\"][@placeholder=\"Ваш e-mail\"]";
     public static final String INPUT_PASSWORD_XPATH = "//input[@type=\"password\"][@placeholder=\"Придумайте пароль\"]";
-    public static final String MESSAGE_TO_ENTER_PASSWORD_MIN_8_CHAR = "//div[@class=\"auth-form__securebox auth-form__securebox_condensed\"][1]";
-    public static final String INPUT_REPEAT_PASSWORD = "//input[@type=\"password\"][@placeholder=\"Повторите пароль\"]";
+    public static final String MESSAGE_TO_ENTER_PASSWORD_MIN_8_CHAR_XPATH = "//div[@class=\"auth-form__securebox auth-form__securebox_condensed\"][1]";
+    public static final String INPUT_REPEAT_PASSWORD_XPATH = "//input[@type=\"password\"][@placeholder=\"Повторите пароль\"]";
 }
