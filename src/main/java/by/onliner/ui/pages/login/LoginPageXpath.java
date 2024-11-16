ac3bc2e0488719a395366a89f52e6ef6277bd2e4
nstsy <nstsy@yandex.ru>
2024-11-16 19:40:06 +0300

    изменила xpath

--- src/main/java/by/onliner/ui/pages/login/LoginPageXpath.java
+++ src/main/java/by/onliner/ui/pages/login/LoginPageXpath.java
@@ -3,7 +3,7 @@ package by.onliner.ui.pages.login;
 public class LoginPageXpath {
     public static final String BUTTON_CLOSE_XPATH = "//div[@class='auth-form__close']";
     public static final String FIELD_TITLE_USERNAME_XPATH = "(//div[@class='auth-form__label-title'])[2]";
-    public static final String INPUT_NAME_XPATH = "//input[@wfd-id='id1']";
-    public static final String INPUT_PASSWORD_XPATH = "//input[@wfd-id='id2']";
+    public static final String INPUT_NAME_XPATH = "//input[@placeholder='Ник или e-mail']";
+    public static final String INPUT_PASSWORD_XPATH = "//input[@placeholder='Пароль']";
     public static final String BUTTON_LOGIN_XPATH = "//button[@type='submit']";
 }
