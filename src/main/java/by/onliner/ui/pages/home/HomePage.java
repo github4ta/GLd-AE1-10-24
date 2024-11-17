a410a751d6f6b7a40bb627cb5138c1235521adbd
Nstsy <nstsy@yandex.ru>
2024-11-17 10:25:42 +0300

    добавлены методы (#52)

--- src/main/java/by/onliner/ui/pages/home/HomePage.java
+++ src/main/java/by/onliner/ui/pages/home/HomePage.java
@@ -1,6 +1,7 @@
 package by.onliner.ui.pages.home;
 
 import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
@@ -9,5 +10,13 @@ public class HomePage {
 
     public HomePage() {
         driver = Driver.getDriver();
+    }
+
+    public String getVacanciesLinkText(){
+        return driver.findElement(By.xpath(HomePageXpath.VACANCIES_XPATH)).getText();
+    }
+
+    public void clickManifestLink(){
+        driver.findElement(By.xpath(HomePageXpath.MANIFEST_XPATH)).click();
     }
 }
