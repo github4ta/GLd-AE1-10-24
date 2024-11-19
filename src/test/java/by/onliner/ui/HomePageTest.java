1a078c583aafefb82181c6adeca08173bb737efd
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-11-19 11:12:45 +0300

    добавлен тест проверки отображения ссылки "Контакты редакции"

--- src/test/java/by/onliner/ui/HomePageTest.java
+++ src/test/java/by/onliner/ui/HomePageTest.java
@@ -1,5 +1,21 @@
 package by.onliner.ui;
 
+import by.onliner.ui.pages.home.HomePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class HomePageTest extends BaseTest {
+
+    @Test
+    @DisplayName("Отображение ссылки 'Контакты редакции' в футере страницы")
+    public void testDisplayEditorContactsLink() {
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Контакты редакции";
+        String actualResult = homePage.getTextEditorContacts();
+
+        Assertions.assertEquals(expectedResult, actualResult, "Неверный текст ссылки" );
+    }
 
 }
