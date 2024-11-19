4858f1194cacddf3791fbbbf36d7d93e5726da8a
Yuliya <yuliya.01707@gmail.com>
2024-11-19 15:03:37 +0100

    add test to check returnPolicy (draft)

--- src/test/java/by/onliner/ui/HomePageTest.java
+++ src/test/java/by/onliner/ui/HomePageTest.java
@@ -1,5 +1,33 @@
 package by.onliner.ui;
 
-public class HomePageTest extends BaseTest {
+import by.onliner.ui.pages.home.HomePage;
+import by.onliner.ui.pages.home.HomePageXpath;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-}
+public class HomePageTest extends BaseTest {
+    private WebDriver driver;
+    private HomePage homepage;
+
+    @BeforeEach
+    public void setup() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.onliner.by/");
+        driver.manage().window().fullscreen();
+        this.driver = driver;
+
+        this.homepage = new HomePage(this.driver);
+
+
+        @Test
+        @DisplayName("Правила возврата в футере на домашней странице")
+        public void returnPolicy() {
+            this.homepage.clickReturnPolicyLink();
+            Assertions.assertEquals(HomePageXpath.FOOTER_LINK_RETURN_POLICY_XPATH, HomePage.getTextReturnPolicy);
+        }
+    }
+    }
