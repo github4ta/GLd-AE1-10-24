8fc92e93546a64a4bf83a2e7e92cd77b1c5385dc
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-19 17:57:21 +0300

    refactor HomePage add testDisplayAdvertisingLink

--- src/test/java/by/onliner/ui/HomePageTest.java
+++ src/test/java/by/onliner/ui/HomePageTest.java
@@ -1,5 +1,18 @@
 package by.onliner.ui;
 
+import by.onliner.ui.pages.home.HomePage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class HomePageTest extends BaseTest {
 
+    @Test
+    @DisplayName("Текст ссылки 'Реклама' отображается в футере")
+    public void testDisplayAdvertisingLink() {
+        HomePage homePage = new HomePage();
+
+        String expectedResult = "Реклама";
+        Assertions.assertEquals(expectedResult, homePage.getTextAdvertisingLink());
+    }
 }
