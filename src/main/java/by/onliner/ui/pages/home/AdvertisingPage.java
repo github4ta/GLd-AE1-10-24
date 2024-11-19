4d0fea77ed167d99e152e240e5220b1233add3d0
baidakm <maksimbaidak@gmail.com>
2024-11-19 15:17:57 +0300

    add constructors

--- src/main/java/by/onliner/ui/pages/home/AdvertisingPage.java
+++ src/main/java/by/onliner/ui/pages/home/AdvertisingPage.java
@@ -1,6 +1,17 @@
 package by.onliner.ui.pages.home;
 
+import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.WebDriver;
+
 public class AdvertisingPage {
 
+    private WebDriver driver;
 
+    public AdvertisingPage(){
+        driver = Driver.getDriver();
+    }
+
+    public AdvertisingPage(WebDriver driver){
+        this.driver = driver;
+    }
 }
