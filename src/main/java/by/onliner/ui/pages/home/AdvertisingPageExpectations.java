1859bd8c07db2c373f1cb2830e5a2f1026635c32
baidakm <maksimbaidak@gmail.com>
2024-11-19 15:28:47 +0300

    add data fields

--- src/main/java/by/onliner/ui/pages/home/AdvertisingPageExpectations.java
+++ src/main/java/by/onliner/ui/pages/home/AdvertisingPageExpectations.java
@@ -2,5 +2,6 @@ package by.onliner.ui.pages.home;
 
 public class AdvertisingPageExpectations {
 
-
+    public static final String URL = "https://b2breg.onliner.by/advertising";
+    public static final String TITLE = "Реклама на сайте Onlíner";
 }
