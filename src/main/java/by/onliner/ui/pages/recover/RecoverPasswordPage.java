0ec5361bd71d43486c8843eaa44a2f10f3f741de
Mits16 <mits1388@gmail.com>
2024-11-25 21:45:58 +0300

    add method sendKeysUserNameRecoverPassword

--- src/main/java/by/onliner/ui/pages/recover/RecoverPasswordPage.java
+++ src/main/java/by/onliner/ui/pages/recover/RecoverPasswordPage.java
@@ -1,7 +1,10 @@
 package by.onliner.ui.pages.recover;
 
 import by.onliner.ui.driver.Driver;
+import by.onliner.ui.pages.login.LoginPageXpath;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class RecoverPasswordPage {
 
@@ -10,5 +13,12 @@ public class RecoverPasswordPage {
     public RecoverPasswordPage() {
         WebDriver driver = Driver.getDriver();
         this.driver = driver;
+    }
+
+    public RecoverPasswordPage sendKeysUserNameRecoverPassword(String login) {
+        By inputUsernameRecoverPasswordBy = By.xpath(RecoverPasswordPageXpath.AUTH_FORM_DATA_INPUT_XPATH);
+        WebElement inputUsernameRecoverPasswordWebElement = driver.findElement(inputUsernameRecoverPasswordBy);
+        inputUsernameRecoverPasswordWebElement.sendKeys(login);
+        return this;
     }
 }
