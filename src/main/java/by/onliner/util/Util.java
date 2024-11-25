2de912518840a68a7a2eacce61d966490f50178e
Pavel <pamitrovel@gmail.com>
2024-11-25 23:13:10 +0300

    refactor RandomStringUtils.randomAscii(7->9,13)

--- src/main/java/by/onliner/util/Util.java
+++ src/main/java/by/onliner/util/Util.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class Util {
     public static String generatePassword(){
-        return RandomStringUtils.randomAscii(7,13);
+        return RandomStringUtils.randomAscii(9,13);
     }
 }
