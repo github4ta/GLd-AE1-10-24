5405db338b2b5ae4134e9f40f3088a1f8591f489
Arseni Aleksandrovich <theintosky@gmail.com>
2024-11-26 00:44:07 +0300

    create RandomNicknameSymbols for generate random chars for nickname. Create generateNick method for generate random nickname

---
+++ src/main/java/by/onliner/ui/utils/socialMediaMethods/RandomNicknameSymbols.java
@@ -0,0 +1,35 @@
+package by.onliner.ui.utils.socialMediaMethods;
+
+public class RandomNicknameSymbols {
+    public static char getRandomChar() {
+        int randomCharCategory = (int) (Math.random() * 3) + 1;
+        if (randomCharCategory == 1) {
+            return getRandomCapitalLetter();
+        } else if (randomCharCategory == 2) {
+            return getLowercaseLetter();
+        } else
+            return getRandomNumber();
+    }
+
+    private static char getRandomCapitalLetter() {
+        int min = 65;
+        int max = 91;
+        int letter = min + (int) (Math.random() * (max - min));
+        return (char) letter;
+    }
+
+    private static char getLowercaseLetter() {
+        int min = 97;
+        int max = 123;
+        int letter = min + (int) (Math.random() * (max - min));
+        return (char) letter;
+
+    }
+
+    private static char getRandomNumber() {
+        int min = 48;
+        int max = 58;
+        int letter = min + (int) (Math.random() * (max - min));
+        return (char) letter;
+    }
+}
--- src/main/java/by/onliner/util/Util.java
+++ src/main/java/by/onliner/util/Util.java
@@ -1,9 +1,21 @@
 package by.onliner.util;
 
+import by.onliner.ui.utils.socialMediaMethods.RandomNicknameSymbols;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class Util {
-    public static String generatePassword(){
-        return RandomStringUtils.randomAscii(9,13);
+    public static String generatePassword() {
+        return RandomStringUtils.randomAscii(9, 13);
+    }
+
+    public static String generateNick() {
+        int min = 3;
+        int max = 9;
+        int stringLength = min + (int) (Math.random() * (max - min));
+        String nick = "";
+        for (int i = 0; i <= stringLength; i++) {
+            nick += RandomNicknameSymbols.getRandomChar();
+        }
+        return nick;
     }
 }
