f8f62d95159fb7bf763a3e7840d73c05729fc790
Pavel <pamitrovel@gmail.com>
2024-11-25 19:29:40 +0300

    add generatePassword

--- src/main/java/by/onliner/util/Util.java
+++ src/main/java/by/onliner/util/Util.java
@@ -1,4 +1,9 @@
 package by.onliner.util;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Util {
+    public static String generatePassword(){
+        return RandomStringUtils.randomAscii(7,13);
+    }
 }
