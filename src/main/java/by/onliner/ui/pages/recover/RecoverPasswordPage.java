8129965bdb79c25b1aa41b4a609528687f9dfd5b
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-25 18:22:26 +0300

    Add method clickButtonNext

--- src/main/java/by/onliner/ui/pages/recover/RecoverPasswordPage.java
+++ src/main/java/by/onliner/ui/pages/recover/RecoverPasswordPage.java
@@ -1,6 +1,7 @@
 package by.onliner.ui.pages.recover;
 
 import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class RecoverPasswordPage {
@@ -10,5 +11,9 @@ public class RecoverPasswordPage {
     public RecoverPasswordPage() {
         WebDriver driver = Driver.getDriver();
         this.driver = driver;
+    }
+
+    public void clickButtonNext() {
+        driver.findElement(By.xpath(RecoverPasswordPageXpath.AUTH_FORM_SUBMIT_BUTTON_XPATH)).click();
     }
 }
