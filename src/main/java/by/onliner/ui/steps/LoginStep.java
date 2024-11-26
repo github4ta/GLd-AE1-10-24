49d5a78d1d6bf8cf5739862d35d169a629c22077
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-11-26 17:27:11 +0300

    add class LoginStep.java

--- src/main/java/by/onliner/ui/steps/LoginStep.java
+++ src/main/java/by/onliner/ui/steps/LoginStep.java
@@ -4,9 +4,16 @@ import by.onliner.ui.pages.login.LoginPage;
 
 public class LoginStep {
 
-    private LoginPage loginPage;
+    private final LoginPage loginPage;
 
     public LoginStep() {
         loginPage = new LoginPage();
+    }
+
+    public void fillLoginFormAndSubmit(String login, String password) {
+        loginPage
+                .fillUsername(login)
+                .fillPassword(password)
+                .clickLoginButton();
     }
 }
