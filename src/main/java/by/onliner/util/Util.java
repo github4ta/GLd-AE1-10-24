256159573050b9dfe5594aa76154ec05b0f6bfa9
Arseni <137502512+FoXCity1337@users.noreply.github.com>
2024-11-26 19:49:39 +0300

    ON-UI-700 - Генератор случайных ников (#96)
    * create RandomNicknameSymbols for generate random chars for nickname. Create generateNick method for generate random nickname
    
    * create RandomNicknameSymbols for generate random chars for nickname. Create generateNick method for generate random nickname
    
    * create RandomNicknameSymbols for generate random chars for nickname. Create generateNick method for generate random nickname
    
    * create RandomNicknameSymbols for generate random chars for nickname. Create generateNick method for generate random nickname

--- src/main/java/by/onliner/util/Util.java
+++ src/main/java/by/onliner/util/Util.java
@@ -1,5 +1,7 @@
 package by.onliner.util;
 
+import by.onliner.ui.utils.randomNicknameMethods.RandomNicknameSymbols;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -31,7 +33,18 @@ public class Util {
         return name + "@" + domain;
     }
 
-    public static String generatePassword(){
-        return RandomStringUtils.randomAscii(9,13);
+    public static String generatePassword() {
+        return RandomStringUtils.randomAscii(9, 13);
+    }
+
+    public static String generateNick() {
+        int min = 3;
+        int max = 9;
+        int stringLength = min + (int) (Math.random() * (max - min));
+        String nick = "";
+        for (int i = 0; i <= stringLength; i++) {
+            nick += RandomNicknameSymbols.getRandomChar();
+        }
+        return nick;
     }
 }
