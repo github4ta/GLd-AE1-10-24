a0ea425e9b3921ac2c118f51b33648abf0ff0c54
palinaerdman <polinaerdman@gmail.com>
2024-11-28 20:11:34 +0300

    add Calculator

--- src/main/java/biz/svyatoslav/pages/CalculatorXpath.java
+++ src/main/java/biz/svyatoslav/pages/CalculatorXpath.java
@@ -5,5 +5,6 @@ public class CalculatorXpath {
     public static final String INPUT_HEIGHT_XPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
     public static final String INPUT_WEIGHT_XPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
     public static final String INPUT_GENDER_FEMALE_XPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[2]";
+    public static final String INPUT_GENDER_MALE_XPATH = "//input[@value='m']";
     public static final String INPUT_CALCULATE_BUTTON_XPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
 }
