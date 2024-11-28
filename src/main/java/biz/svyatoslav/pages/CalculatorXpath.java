bd419b4c599cba6c8755601c9bb5f94d2a18350f
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-28 21:02:54 +0300

    add messages

--- src/main/java/biz/svyatoslav/pages/CalculatorXpath.java
+++ src/main/java/biz/svyatoslav/pages/CalculatorXpath.java
@@ -7,4 +7,5 @@ public class CalculatorXpath {
     public static final String INPUT_GENDER_FEMALE_XPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[2]";
     public static final String INPUT_GENDER_MALE_XPATH = "//input[@value='m']";
     public static final String INPUT_CALCULATE_BUTTON_XPATH = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+    public static final String ERROR_MESSAGE_XPATH = "//b";
 }
