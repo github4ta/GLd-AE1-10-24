1ca586cb451b51a73758d7c74f0c86e348521bca
Sergei Tsarik <serge.tsarik@gmail.com>
2024-11-28 22:16:32 +0300

    add api object

---
+++ src/test/java/biz/svyatoslav/api/CalculatorApiTest.java
@@ -0,0 +1,17 @@
+package biz.svyatoslav.api;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorApiTest {
+    @Test
+    @DisplayName("Не заданы параметры в body")
+    public void testEmptyBody() {
+        String EMPTY_VLAUE = "";
+        CalculatorApi calculatorApi = new CalculatorApi();
+        ValidatableResponse response = calculatorApi.getResponseForRequestWithData(EMPTY_VLAUE, EMPTY_VLAUE, EMPTY_VLAUE);
+
+        response.statusCode(200);
+    }
+}
