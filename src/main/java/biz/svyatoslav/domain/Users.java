82ab12c41937e26f943be261308c3dd50cbd8298
nstsy <nstsy@yandex.ru>
2024-11-29 21:56:58 +0300

    добавлен getUserWithNameAndWeight

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,10 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+
+    public NewUser getUserWithNameAndWeight() {
+        return new NewUser("Bob", 0, 80, Gender.MALE);
+    }
 }
