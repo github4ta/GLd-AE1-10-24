1991ff9435a560caaabd8d5d1291726caa714d30
PavelMitrokhin <pamitrovel@gmail.com>
2024-11-29 20:39:05 +0300

    add testEmptyBody2 (#114)

--- src/test/java/biz/svyatoslav/api/CalculatorApiTest.java
+++ src/test/java/biz/svyatoslav/api/CalculatorApiTest.java
@@ -1,5 +1,6 @@
 package biz.svyatoslav.api;
 
+import biz.svyatoslav.domain.User;
 import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,17 @@ public class CalculatorApiTest {
         String EMPTY_VLAUE = "";
         CalculatorApi calculatorApi = new CalculatorApi();
         ValidatableResponse response = calculatorApi.getResponseForRequestWithData(EMPTY_VLAUE, EMPTY_VLAUE, EMPTY_VLAUE, EMPTY_VLAUE);
+
+        response.statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Не заданы параметры в body")
+    public void testEmptyBody2() {
+        String EMPTY_VLAUE = "";
+        User user = new User(EMPTY_VLAUE, 0, 0, EMPTY_VLAUE);
+        CalculatorApi calculatorApi = new CalculatorApi();
+        ValidatableResponse response = calculatorApi.getResponseForRequestWithData(user);
 
         response.statusCode(200);
     }
