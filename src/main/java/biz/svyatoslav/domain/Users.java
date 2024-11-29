259ccf890a769a82e2afd726057e14464fec084f
Sergei Tsarik <serge.tsarik@gmail.com>
2024-11-29 21:54:16 +0300

    add getValidUser

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,10 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+
+    public NewUser getUserWithValidData() {
+        return new NewUser("Name", 180, 80, Gender.MALE);
+    }
 }
