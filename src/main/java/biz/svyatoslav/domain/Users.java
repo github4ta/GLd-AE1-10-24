6b7461cb16db1bd0f0f81c3cd73c6f3e37b23f46
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-29 22:02:35 +0300

    get Users

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,9 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+    public NewUser getUserDataWithHeight() {
+        return new NewUser("Eugene", 0, 0, Gender.MALE);
+    }
 }
