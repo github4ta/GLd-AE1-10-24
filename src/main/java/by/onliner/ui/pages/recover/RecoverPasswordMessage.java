ac3bf3ec44dc67e44def3f19bd3f46d74c2529f5
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-29 18:13:05 +0300

    add file RecoverPasswordMessage

---
+++ src/main/java/by/onliner/ui/pages/recover/RecoverPasswordMessage.java
@@ -0,0 +1,5 @@
+package by.onliner.ui.pages.recover;
+
+public class RecoverPasswordMessage {
+    public static final String ERROR_MESSAGE_EMPTY_LOGIN = "Укажите ник, e-mail или номер телефона";
+}
