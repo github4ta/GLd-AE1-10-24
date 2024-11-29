90857d02b59fc305c4d49ad68d1fbcab0ede01ef
Pavel <pamitrovel@gmail.com>
2024-11-29 22:01:30 +0300

    add getZeroWeightUser

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,10 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+    public NewUser getZeroWeightUser(){
+        NewUser newUser =  new NewUser("Benya", 180, 0, Gender.MALE);
+        return newUser;
+    }
 }
