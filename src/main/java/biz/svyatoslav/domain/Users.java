403f81273fb6a8e72a447bc2ed2711a0f5642377
tolbist4@gmail.com <AnatoliyStavskiy>
2024-11-29 21:59:20 +0300

    add user

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,9 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+    public NewUser getUserDataWithHeight() {
+        return new NewUser("", 180, 0, Gender.MALE);
+    }
 }
