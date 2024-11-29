f9f39745199cdfe4c6e29896abc018717d7760f1
Yuliya <yuliya.01707@gmail.com>
2024-11-29 19:57:44 +0100

    add method getUserWithNoData

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,10 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+    public NewUser getUserWithNoData() {
+       NewUser newUser = new NewUser("", 0, 0, Gender.MALE);
+       return newUser;
+    }
 }
