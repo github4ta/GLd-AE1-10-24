eb2c4447f160714600ca148e98f93281c8595395
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-29 18:13:25 +0300

    add file RecoverPasswordPage

---
+++ src/main/java/by/onliner/ui/pages/recover/RecoverPasswordPage.java
@@ -0,0 +1,38 @@
+package by.onliner.ui.pages.recover;
+
+import by.onliner.ui.driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RecoverPasswordPage {
+    private WebDriver driver;
+
+    public RecoverPasswordPage() {
+        driver = Driver.getDriver();
+        this.driver = driver;
+    }
+
+    public RecoverPasswordPage clickSignInButton() {
+        driver.findElement(By.xpath(RecoverPasswordPageXpath.BUTTON_OPEN_FORM_LOGIN_XPATH)).click();
+        return this;
+    }
+
+    public RecoverPasswordPage clickOpenFormRecoverPassword() {
+        driver.findElement(By.xpath(RecoverPasswordPageXpath.BUTTON_OPEN_FORM_RECOVER_PASSWORD_XPATH)).click();
+        return this;
+    }
+
+    public RecoverPasswordPage inputLogin(String login) {
+        driver.findElement(By.xpath(RecoverPasswordPageXpath.AUTH_FORM_DATA_INPUT_XPATH)).sendKeys(login);
+        return this;
+    }
+
+    public RecoverPasswordPage clickButtonNext() {
+        driver.findElement(By.xpath(RecoverPasswordPageXpath.AUTH_FORM_SUBMIT_BUTTON_XPATH)).click();
+        return this;
+    }
+
+    public String getTextMessageLoginEmpty() {
+        return driver.findElement(By.xpath(RecoverPasswordPageXpath.AUTH_FORM_DESCRIPTION_EMPTY_LOGIN)).getText();
+    }
+}
