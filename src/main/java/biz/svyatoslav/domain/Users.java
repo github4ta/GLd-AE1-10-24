3b376a200cf9a27be4c756a00325b01f0993108b
ViktoriaZakarian <zakarianviktoria@gmail.com>
2024-11-29 21:58:44 +0300

    add getUserWithNoNameNoHeightValidWeightGenderMale() (#118)

--- src/main/java/biz/svyatoslav/domain/Users.java
+++ src/main/java/biz/svyatoslav/domain/Users.java
@@ -1,4 +1,11 @@
 package biz.svyatoslav.domain;
 
+import biz.svyatoslav.enums.Gender;
+
 public class Users {
+
+    public NewUser getUserWithNoNameNoHeightValidWeightGenderMale() {
+        NewUser user = new NewUser("", 0, 80, Gender.MALE);
+        return user;
+    }
 }
